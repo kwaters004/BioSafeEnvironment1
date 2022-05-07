b270a8884d6628eb708ca91696a18eca0f2a65a6
k_wat <k_wat@DESKTOP-OI42D3A>
2022-05-07 09:01:29 -0400

    Add project files.

---
+++ BioSafeEnvironment1/ClientApp/src/app/about-us/about-us.component.spec.ts
@@ -0,0 +1,25 @@
+﻿/// <reference path="../../../../node_modules/@types/jasmine/index.d.ts" />
+import { TestBed, async, ComponentFixture, ComponentFixtureAutoDetect } from '@angular/core/testing';
+import { BrowserModule, By } from "@angular/platform-browser";
+import { AboutUsComponent } from './about-us.component';
+
+let component: AboutUsComponent;
+let fixture: ComponentFixture<AboutUsComponent>;
+
+describe('about-us component', () => {
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [ AboutUsComponent ],
+            imports: [ BrowserModule ],
+            providers: [
+                { provide: ComponentFixtureAutoDetect, useValue: true }
+            ]
+        });
+        fixture = TestBed.createComponent(AboutUsComponent);
+        component = fixture.componentInstance;
+    }));
+
+    it('should do something', async(() => {
+        expect(true).toEqual(true);
+    }));
+});
